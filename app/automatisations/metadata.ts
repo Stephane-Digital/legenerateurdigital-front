e6f774009890c086b1680344b5f521155dfed6aa
Stephane-Digital <contact@first-digital-academy.com>
2025-10-17 15:05:39 +0200

    Create metadata.ts

---
+++ app/automatisations/metadata.ts
@@ -0,0 +1,7 @@
+// app/automatisations/metadata.ts
+export const metadata = {
+  title: "Automatisations | LeGenerateurDigital",
+  description: "Gérez vos automatisations (webhooks, intégrations, envois) depuis votre espace.",
+};
+
+export default {};
