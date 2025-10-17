c67a61037fb4b238aa6af2a6781872b0144b036e
Stephane-Digital <contact@first-digital-academy.com>
2025-10-17 09:43:02 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,14 @@
+// app/layout.tsx
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: "LeGenerateurDigital",
+    template: "%s | LeGenerateurDigital",
+  },
+  description: "LeGenerateurDigital",
+};
+
 export default function RootLayout({
   children,
 }: {
