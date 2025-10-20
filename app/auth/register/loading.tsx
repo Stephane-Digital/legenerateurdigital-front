54bd99e3d1656b2200bf168674ae19bcad5a328d
Stephane-Digital <contact@first-digital-academy.com>
2025-10-20 07:53:54 +0200

    Add loading component for registration page

---
+++ app/auth/register/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div style={{ color: "white", textAlign: "center", padding: "40px" }}>
+      Chargement de la page d’inscription...
+    </div>
+  );
+}
