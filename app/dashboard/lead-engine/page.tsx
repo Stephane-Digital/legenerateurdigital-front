79055a5baecb757c878cc5a49ce769bfbc76c0f4
Stephane-Digital <contact@first-digital-academy.com>
2026-04-03 08:25:09 +0200

    Update page.tsx

--- app/dashboard/lead-engine/page.tsx
+++ app/dashboard/lead-engine/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import { useEffect, useMemo, useState } from "react";
 
 import CardLuxe from "@/components/ui/CardLuxe";
-import LeadEngineBlock from "./components/LeadEngineBlock";
+import LeadEngineBlock from "../components/LeadEngineBlock";
 
 // Icons (react-icons/fa)
 import {
