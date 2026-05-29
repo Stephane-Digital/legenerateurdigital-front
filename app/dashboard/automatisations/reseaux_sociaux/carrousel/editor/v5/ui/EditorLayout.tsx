6c8a9d960e3b28f3739df2bb46bfcce90d2dab93
Stephane-Digital <contact@first-digital-academy.com>
2026-05-29 14:35:44 +0200

    Update EditorLayout.tsx

--- app/dashboard/automatisations/reseaux_sociaux/carrousel/editor/v5/ui/EditorLayout.tsx
+++ app/dashboard/automatisations/reseaux_sociaux/carrousel/editor/v5/ui/EditorLayout.tsx
@@ -1457,7 +1457,7 @@ export default function EditorLayout({
                       : "border-yellow-500/20 text-yellow-200"
                   }`}
                 >
-                  {overlayEnabled ? "désctivé" : "désctiver"}
+                  {overlayEnabled ? "Activé" : "Activer"}
                 </button>
               </div>
 
